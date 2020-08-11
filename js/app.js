f465ccf30ad1914c40149de0b167736d97c59184
SGS-J <santigarsan07@gmail.com>
2020-08-11 14:04:15 -0500

    Acomodamos los archivos JS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-'use strict'
+"use strict";
 
-var inputNumerador;
+let inputs;
 var numerador;
-var inputDenominador;
 var denominador;
 var botonSimplificar;
 var botonReiniciar;
@@ -13,99 +12,62 @@ var numeroInvalido;
 
 // Iniciamos scripts
 window.onload = () => {
-   obtenerDocumento();
-   botonSimplificar.onclick = () => simplificar();
-   botonReiniciar.onclick = () => reiniciar();
-   resultadoNumerador.hidden = true;
-   resultadoDenominador.hidden = true;
-   botonReiniciar.hidden = true;
-   document.querySelector('main').style.opacity = '1';
-   camposVacios = true;
-   numeroInvalido = true;
-}
+  obtenerDocumento();
+  configurarInputs();
+  // TODO: Colocar función ocultar resultados
+  document.querySelector("main").style.opacity = "1";
+  camposVacios = true;
+  numeroInvalido = true;
+};
 
 function obtenerDocumento() {
-   inputNumerador = document.querySelector('.input-numerador');
-   inputDenominador = document.querySelector('.input-denominador');
-   botonSimplificar = document.querySelector('.boton-resolver');
-   resultadoNumerador = document.querySelector('.resolvedor__resultado--numerador')
-   resultadoDenominador = document.querySelector('.resolvedor__resultado--denominador');
-   botonReiniciar = document.querySelector('.resolvedor__resultado--reiniciar');
+  inputs = document.querySelectorAll(".resolvedor__resolucion--input");
+  botonSimplificar = document.querySelector(".boton-resolver");
+  resultadoNumerador = document.querySelector(
+    ".resolvedor__resultado--numerador"
+  );
+  resultadoDenominador = document.querySelector(
+    ".resolvedor__resultado--denominador"
+  );
+  botonReiniciar = document.querySelector(".resolvedor__resultado--reiniciar");
 }
 
-function simplificar() {
-   if (listoParaSimplificar()) {
-      realizarAlgoritmo();
-      mostrarResultado();
-   } else {
-      mostrarError();
-      reiniciar();
-   }
+function configurarInputs() {
+  for (let i = 0; i < inputs.length; i++) {
+    const element = inputs[i];
+    if (i > 1) {
+      element.addEventListener("click", evento.simplificar);
+    } else {
+      element.addEventListener("keypress", (e) => {
+        return (e.charCode >= 48 && e.charCode <= 57) || e.charCode == 45;
+      });
+    }
+  }
 }
 
-function listoParaSimplificar() {
-   if(inputNumerador.value == '' || inputDenominador.value == '' ) {
-      camposVacios = true;
-   } else {
-      camposVacios = false;
-   }
-   if(parseInt(inputNumerador.value) > 9999 || parseInt(inputDenominador.value) > 9999) {
-      numeroInvalido = true;
-   } else {
-      numeroInvalido = false;
-   }
-   return !camposVacios && !numeroInvalido;
-}
-
-function realizarAlgoritmo() {
-   // Usamos algoritmo de euclides(Para conseguir el MCD de los dos numeros)
-   numerador = inputNumerador.value;
-   denominador = inputDenominador.value;
-   let mcd = algoritmoEuclides.calcularMCD(numerador, denominador);
-   // Simplificamos
-   numerador /= mcd;
-   denominador /= mcd;
-}
-
-function mostrarResultado() {
-   resultadoNumerador.hidden = false;
-   resultadoDenominador.hidden = false;
-   botonReiniciar.hidden = false;
-   resultadoNumerador.innerHTML = numerador;
-   resultadoDenominador.innerHTML = denominador;
-}
-
-function reiniciar() {
-   resultadoNumerador.hidden = true;
-   resultadoDenominador.hidden = true;
-   botonReiniciar.hidden = true;
-   inputNumerador.value = '';
-   inputDenominador.value = '';
-   numerador = 0;
-   denominador = 0;
-}
-
-function mostrarError() {
-   alert('Ingrese campos validos');
+function ocultarResultados() {
+  resultadoNumerador.hidden = true;
+  resultadoDenominador.hidden = true;
+  botonReiniciar.hidden = true;
 }
 
 class AlgoritmoEuclides {
-   constructor() {
-     this.calcularMCD = (numeroA, numeroB) => {
-       // Primeros organizamos los numeros de mayor a menor si es necesario
-       if (numeroA < numeroB) {
-         numeroA = numeroA + numeroB;
-         numeroB = numeroA - numeroB;
-         numeroA = numeroA - numeroB;
-       }
-       // Luego aplicamos recursion
-       if (numeroA % numeroB == 0) {
-         return numeroB;
-       } else {
-         return this.calcularMCD(numeroB, numeroA % numeroB);
-       }
-     };
-   }
- }
- 
- const algoritmoEuclides = new AlgoritmoEuclides();
+  constructor() {
+    this.calcularMCD = (numeroA, numeroB) => {
+      // Primeros organizamos los numeros de mayor a menor si es necesario
+      if (numeroA < numeroB) {
+        numeroA = numeroA + numeroB;
+        numeroB = numeroA - numeroB;
+        numeroA = numeroA - numeroB;
+      }
+      // Luego aplicamos recursion
+      if (numeroA % numeroB == 0) {
+        return numeroB;
+      } else {
+        return this.calcularMCD(numeroB, numeroA % numeroB);
+      }
+    };
+  }
+}
+
+const algoritmoEuclides = new AlgoritmoEuclides();
---
+++ js/eventos.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const evento = new Evento();
+
+class Evento {
+  constructor() {
+    this.simplificar = () => {
+      if (listoParaSimplificar()) {
+        realizarAlgoritmo();
+        mostrarResultado();
+      } else {
+        mostrarError();
+        reiniciar();
+      }
+    };
+    this.reiniciar = () => reiniciar();
+  }
+}
+
+function listoParaSimplificar() {
+  if (inputNumerador.value == "" || inputDenominador.value == "") {
+    camposVacios = true;
+  } else {
+    camposVacios = false;
+  }
+  return !camposVacios;
+}
+
+function realizarAlgoritmo() {
+  // Usamos algoritmo de euclides(Para conseguir el MCD de los dos numeros)
+  numerador = inputNumerador.value;
+  denominador = inputDenominador.value;
+  let mcd = algoritmoEuclides.calcularMCD(numerador, denominador);
+  // Simplificamos
+  numerador /= mcd;
+  denominador /= mcd;
+}
+
+function mostrarResultado() {
+  resultadoNumerador.hidden = false;
+  resultadoDenominador.hidden = false;
+  botonReiniciar.hidden = false;
+  resultadoNumerador.innerHTML = numerador;
+  resultadoDenominador.innerHTML = denominador;
+}
+
+function reiniciar() {
+  resultadoNumerador.hidden = true;
+  resultadoDenominador.hidden = true;
+  botonReiniciar.hidden = true;
+  inputNumerador.value = "";
+  inputDenominador.value = "";
+  numerador = 0;
+  denominador = 0;
+}
+
+function mostrarError() {
+  alert("Ingrese campos validos");
+}
