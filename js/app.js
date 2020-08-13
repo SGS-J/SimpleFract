9dfe4f41b30d3e4f4eb1df237b36406100e31511
SGS-J <santigarsan07@gmail.com>
2020-08-13 09:17:57 -0500

    Ajustado diseño de inputs

--- js/app.js
+++ js/app.js
@@ -7,31 +7,34 @@ let mensajeResultados;
 
 window.addEventListener("load", () => {
   obtenerDocumento();
+  //ocultarResultados();
   configurarEventos();
-  ocultarResultados();
   animarEntrada();
 });
 
 function obtenerDocumento() {
-  inputs = document.querySelectorAll(".resolvedor__resolucion input[type='text']");
+  inputs = document.querySelectorAll(
+    ".resolvedor__resolucion input[type='text']"
+  );
   botonSimplificar = document.querySelector(".boton-resolver");
-  botonReiniciar = document.querySelector('.resolvedor__resultado--reiniciar');
-  mensajeResultados = document.querySelectorAll('resolvedor__resultado-span');
+  botonReiniciar = document.querySelector(".resolvedor__resultado--reiniciar");
+  mensajeResultados = document.querySelectorAll("resolvedor__resultado-span");
 }
 
 function configurarEventos() {
   for (let i = 0; i < inputs.length; i++) {
     const input = inputs[i];
-    input.addEventListener('keypress', evento.limitarSoloNumeros);
-    input.addEventListener('keypress', evento.verificarMenos);
-    input.addEventListener('paste', evento.verificarCopia);
+    input.addEventListener("keypress", evento.limitarSoloNumeros);
+    input.addEventListener("keypress", evento.verificarMenos);
+    input.addEventListener("paste", evento.verificarCopia);
   }
-  botonSimplificar.addEventListener('click', evento.simplificar);
+  botonSimplificar.addEventListener("click", evento.simplificar);
   botonReiniciar.addEventListener("click", evento.reiniciar);
 }
 
 function ocultarResultados() {
-  const cajaResultado = document.querySelector(".resolvedor__resultado").children;
+  const cajaResultado = document.querySelector(".resolvedor__resultado")
+    .children;
   for (let i = 0; i < cajaResultado.length; i++) {
     const elemento = cajaResultado[i];
     elemento.setAttribute("style", "opacity: 0");
@@ -39,7 +42,10 @@ function ocultarResultados() {
 }
 
 function animarEntrada() {
-  const introduccion = document.querySelector("#introducion");
-  introduccion.setAttribute("style", "margin-top: 0");
-  introduccion.setAttribute("style", "opacity: 1");
+  const introduccion = document.querySelector("main #introduccion");
+  introduccion.style.transform = "translateY(0)";
+  introduccion.style.opacity = "1";
+  const resolvedor = document.querySelector("main #resolvedor");
+  resolvedor.style.transform = "translateY(0)";
+  resolvedor.style.opacity = "1";
 }
